046daa5f796f656d45b4579c8cb196acca3c6948
Nma <ncmbeledogu@gmail.com>
2022-05-23 23:58:47 -0400

    style improvements

--- index/index.js
+++ index/index.js
@@ -11,30 +11,27 @@ function copyToClipboard() {
 
   button.style.width = '4em';
   button.innerText = 'copied';
-  button.style.background = 'rgb(52, 199, 89)';
-  button.style.color = 'white';
+  button.style.background = 'var(--green)';
 
   document.addEventListener('click', () => {
     button.innerText = 'copy';
     button.style.width = '3em';
-    button.style.background = 'rgb(174, 174, 178)';
-    button.style.color = 'black';
+    button.style.background = 'var(--blue)';
   }, { capture: true, once: true });
 
   document.addEventListener('keydown', () => {
     button.innerText = 'copy';
     button.style.width = '3em';
-    button.style.background = 'rgb(174, 174, 178)';
-    button.style.color = 'black';
+    button.style.background = 'var(--blue)';
   }, { capture: true, once: true });
 }
 
 ///////////
 // MODAL //
 ///////////
-let modalOverlay = document.getElementById('modal-overlay');
-let modalFirstElement = document.getElementById('close-about');
-let modalLastElement = document.getElementById('modal-last-focusable');
+const modalOverlay = document.getElementById('modal-overlay');
+const modalFirstElement = document.getElementById('close-about');
+const modalLastElement = document.getElementById('modal-last-focusable');
 
 // trap focus in modal
 modalFirstElement.addEventListener('keydown', (e) => {
@@ -77,6 +74,7 @@ let closeAboutWithEsc = (e) => {
 
 function closeAbout() {
   document.removeEventListener('keydown', closeAboutWithEsc);
+  document.getElementById('modal-content').scrollTo(0, 0);
   modalOverlay.style.display = 'none';
-  document.getElementById('open-about').focus();
+  document.getElementById('read-more').focus();
 }
